d3f9a065a3abc33be1736920faf87ef49ffe563d
will <willzh@live.cn>
2023-11-03 13:15:56 +0800

    add support hls

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'package:flutter_tiktok/pages/homePage.dart';
-import 'package:flutter_tiktok/style/style.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tiktok/pages/homePage.dart';
+import 'package:flutter_tiktok/style/style.dart';
 
 void main() {
   /// 自定义报错页面
@@ -29,15 +29,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         brightness: Brightness.dark,
         hintColor: Colors.white,
-        accentColor: Colors.white,
         primaryColor: ColorPlate.orange,
-        primaryColorBrightness: Brightness.dark,
         scaffoldBackgroundColor: ColorPlate.back1,
         dialogBackgroundColor: ColorPlate.back2,
-        accentColorBrightness: Brightness.light,
-        textTheme: TextTheme(
-          bodyText1: StandardTextStyle.normal,
-        ),
       ),
       home: HomePage(),
     );
